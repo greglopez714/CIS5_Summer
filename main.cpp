5cb85ab56893a3b2e2118678e4d4003f8b3c3c32
greglopez714 <109172827+greglopez714@users.noreply.github.com>
2022-07-12 11:45:15 -0700

    adding a test file
    adding a test file

---
+++ main.cpp
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: 2945621
+ *
+ * Created on July 7, 2022, 10:19 AM
+ */
+
+#include <cstdlib>
+#include<fstream>
+#include<iomanip>
+#include<>
+
+using namespace std;
+nRand=100;
+filName="random.dat";
+out.open(filname,ios::out)
+
+
+for (random=rand(%)90+10;//10-100
+        mxnum,//Maxinum number in the file 
+        mnNum,//Mininum number in the file
+        sum;  // Sum of all the numbers in a file 
+        float average; //Average of the numbers in the file
+        
+
+
+in>>random;
+nxNum=mnNum=random;
+nRand==;
+while(in>>random)
+    if(mxNum<random)mxNum=random;
+    if(mnNum>random)mnNum=random;
+sum+random;
+nRand++
+    
+average=sum/nRand;
+
+cout<<"The"
+        
+
+/*
+ * out.close()
+ */
+int main(int argc, char** argv) {
+    fstream out;//File stream containing the random data 
+    string filName;// Name of output stream file 
+    int random;//Random number to print in file 
+    
+    return 0;
+}
+
